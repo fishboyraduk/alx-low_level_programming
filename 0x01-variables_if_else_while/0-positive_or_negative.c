80c533bd0bd57e2cebbef8acce013c64e6db4591
fishboyraduk <rjayson@protonmail.com>
2022-09-22 08:25:43 -0700

    .

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -5,7 +5,7 @@
 /**
  *main - entry point
  *
- *Return : Always 0
+ *Return: Always 0
  */
 
 int main(void)
@@ -14,12 +14,13 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	
+
 	if (n > 0)
 		printf("%d is positive\n", n);
 	else if (n == 0)
 		printf("%d is zero\n", n);
 	else
 		printf("%d is negative\n", n);
-	return(0);
+
+	return (0);
 }
