da667c0f57eb7a5bef2b20a23ecfa493a8dc5633
fishboyraduk <rjayson@protonmail.com>
2022-09-22 05:43:57 -0700

    .

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
- *prints "programming is like building a multilingual puzzle" 
- *returns 0
+ *main - Prints "Programming is like building a multilingual puzzle"
+ *Return: 0
  */
 
 int main(void)
