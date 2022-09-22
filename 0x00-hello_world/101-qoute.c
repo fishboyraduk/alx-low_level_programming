2fe865c3fc377d0776183f4612bc07007cdf8990
fishboyraduk <rjayson@protonmail.com>
2022-09-22 06:29:28 -0700

    .

---
+++ 0x00-hello_world/101-qoute.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ * return: always 1
+ */
+int main(void)
+{
+	write(STDOUT_FILENO, "and that piece of art is useful
+			\" -Dora Korpar, 2015-10-19\n",59);
+	return(1);
+}
