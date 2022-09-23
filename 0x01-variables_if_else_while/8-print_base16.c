ad7db6fd5abe5b0b75579667c2e777bfe51ca58e
fishboyraduk <rjayson@protonmail.com>
2022-09-23 15:00:35 -0700

    .

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main (void)
+{
+	char low;
+
+	for (low = 'z'; low >= 'a'; low--)
+	{
+		putchar(low);
+	}
+	putchar('\n');
+
+	return (0);
+}
