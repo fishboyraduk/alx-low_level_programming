8cb08a3d9b17b4d35c66bb1a0f2263c4732e8856
fishboyraduk <rjayson@protonmail.com>
2022-09-23 14:40:50 -0700

    .

---
+++ 0x01-variables_if_else_while/5-print_number.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = -; i<10; i++)
+	{
+		printf("%d", i);
+	}
+	printf("\n");
+
+	retunr (0);
+}
