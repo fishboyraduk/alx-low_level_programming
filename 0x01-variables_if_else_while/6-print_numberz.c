563258931ee201a29946ee8eb8962894ba3a78b4
fishboyraduk <rjayson@protonmail.com>
2022-09-23 14:49:51 -0700

    .

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#inlcude <time.h>
+
+/**
+ * main - Entry point
+ * Retun: Always 0
+ */
+
+int main(void)
+{
+	int d;
+
+	for (d = '0'; d <= '9'; d++)
+	{
+		putchar(d);
+	}
+	putchar('\n');
+
+	retunr (0);
+}
