17cf554b6d038d0db5b1abf5d3bba12296f93995
fishboyraduk <rjayson@protonmail.com>
2022-09-23 15:26:45 -0700

    .

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -19,14 +19,13 @@ int main(void)
 			{
 				putchar(d);
 				putchar(p);
-				
-				if (d == '8' && p == '9')
+			}
+			if (d == '8' && p == '9')
 				{
 					continue;
 				}
 				putchar(',');
 				putchar(' ');
-			}
 		}
 	}
 	putchar('\n');
